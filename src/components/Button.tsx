9f7a23d38f71eb153bf3a0590783c927c65738a5
IVainqueur <ishimvainqueur@gmail.com>
2022-07-17 12:02:17 +0200

    nothing special

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+interface ButtonProps {
+    children: JSX.Element | string;
+    rounded: boolean,
+    outlined: boolean,
+    filled: boolean,
+    background: string,
+    color: string
+}
+
+const Button = ({ children, rounded, outlined, filled, background, color }: Partial<ButtonProps>) => {
+    background = background ? background : 'cgreen-300'
+    color = color ? color : 'cgreen-300'
+    // const styles = `cursor-pointer ${rounded && 'rounded'} ${outlined && `${'text-'+color} ${'bg-'+background} border ${'border-'+color} ${'hover:bg-'+color} ${'hover:text-'+background}`} ${filled && `${'text-'+background} ${'bg-'+color} border ${'border-'+background} hover:${'bg-'+background} hover:${'text-'+color}`}
+    // px-7 py-3`
+    const styles = `cursor-pointer px-7 py-3 ${rounded ? 'rounded': ''} ${outlined ? ('text-'+color+' bg-'+background+' border border-'+color+' hover:bg-'+color) : ''}`
+    // console.log(color, background)
+    return (
+    <div className={buildClass({rounded, outlined, filled, background, color})}>
+        {children}
+    </div>
+    );
+};
+
+function buildClass({rounded, outlined, filled, background, color}: Partial<ButtonProps>): string{
+    let styles = 'cursor-pointer px-7 py-3';
+    rounded && (styles +=' rounded');
+    outlined && (styles += ' text-'+color+' bg-'+background+' border border-'+color+' hover:bg-'+color);
+    filled && (styles += ' scale-150');
+    console.log(styles)
+    return styles.toString();
+
+}
+
+export default Button;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,55 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Button from "./Button";
 
-const Navbar  = () =>  {
-  return (
-    <div className="text-white bg-cgreen-200">
-        <div className="Left">
-            <Link to={'/'}>Logo</Link>
-            <div className="links flex items-center">
-                <Link to={'/accomodation'}>Accomodations</Link>
-                <Link to={'/restaurants'}>Restaurants</Link>
-                <Link to={'/transport'}>Transport</Link>
+const Navbar = () => {
+    return (
+        <div className="text-white bg-cgreen-200 flex items-center justify-evenly gap-4 pt-5 pb-4">
+            <div className="Left">
+                <Link to={"/"} className="text-3xl font-bold">Logo</Link>
+                <div className="links flex items-center gap-2">
+                    <Link to={"/accomodation"} className="">
+                        Accomodations
+                    </Link>
+                    <Link to={"/restaurants"}>Restaurants</Link>
+                    <Link to={"/transport"}>Transport</Link>
+                </div>
+            </div>
+            <div className="Right flex flex-row items-center gap-2">
+                <div className="country rounded-full h-10 w-10 bg-cgreen-300 border border-white"></div>
+
+                <Link to={"/assets"}>
+                    <Button
+                        outlined={true}
+                        filled={false}
+                        color="red-200"
+                        background="cgreen-200"
+                        rounded={true}>
+                        List your asset
+                    </Button>
+                </Link>
+                <Link to={"/assets"}>
+                    <Button
+                        outlined={true}
+                        filled={false}
+                        color="red-200"
+                        background="cgreen-200"
+                        rounded={true}>
+                        Register
+                    </Button>
+                </Link>
+                <Link to={"/assets"}>
+                    <Button
+                        outlined={true}
+                        filled={false}
+                        color="red-200"
+                        background="cgreen-200"
+                        rounded={true}>
+                        Login
+                    </Button>
+                </Link>
             </div>
         </div>
-        <div className="Right">
+    );
+};
 
-        </div>
-
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
